97d051df0a777d9a7772f6afb214d398b366f850
Leonardo Martinez Garcia <lendomga@gmail.com>
2022-03-23 23:55:26 -0600

    Prueba1

--- PythonToGit/pythontogit.py
+++ PythonToGit/pythontogit.py
@@ -1,3 +1,10 @@
-from os import system
+import os 
 
-system("flutter doctor")
+temp = os.popen('git push').read()
+
+print("Hola: ", temp)
+
+# import subprocess
+# subprocess.run(["python", "--version"])
+# # Python 3.6.0
+# subprocess.CompletedProcess(args=['python', '--version'], returncode=0)
